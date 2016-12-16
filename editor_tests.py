9d63b66c2494ca57e4a63466f64e37d5362c6646
GodloveD <davidgodlove@gmail.com>
2016-12-16 09:07:43 -0500

    dummy commit for travis testing 2

--- editor_tests.py
+++ editor_tests.py
@@ -1,13 +1,13 @@
-from asciinema.asciicast import load
-from asciinema import editor
-
-ifile = '/home/godlovedc/lolcat.json'
-ofile = '/home/godlovedc/lolcat2.json'
-new_min = 2
-new_max = 0.1
-clip    = ':'
-
-asciicast = load(ifile)
-asciicast = editor.new_min(asciicast, new_min)
-asciicast.save(ofile)
-# dummy commit for travis test
+## from asciinema.asciicast import load
+## from asciinema import editor
+## 
+## ifile = '/home/godlovedc/lolcat.json'
+## ofile = '/home/godlovedc/lolcat2.json'
+## new_min = 2
+## new_max = 0.1
+## clip    = ':'
+## 
+## asciicast = load(ifile)
+## asciicast = editor.new_min(asciicast, new_min)
+## asciicast.save(ofile)
+## # dummy commit for travis test
